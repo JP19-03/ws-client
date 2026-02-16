9d73bcfb5bb0f03cb89559f4ea563cfa03c4e8cc
JP19-03 <u202014511@upc.edu.pe>
2026-02-16 12:43:06 -0500

    feat: enhance client UI with message handling and server status updates

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,15 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <h1>Websocket - Client</h1>
 
     <span id="server-status">Offline</span>
+
+    <ul id="clients-ul"></ul>
+
+    <form id="message-form">
+      <input type="text" placeholder="Message to server" id="message-input">
+    </form>
+
+    <h3>Messages</h3>
+    <ul id="messages-ul"></ul>
   </div>
 `
 
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -11,6 +11,10 @@ export const connectToServer = () => {
 
 const addListeners = (socket: Socket) => {
     const serverStatusLabel = document.querySelector('#server-status')!;
+    const clientsUl = document.querySelector('#clients-ul')!;
+    const messageForm = document.querySelector<HTMLFormElement>('#message-form')!;
+    const messageInput = document.querySelector<HTMLInputElement>('#message-input')!;
+    const messagesUl = document.querySelector('#messages-ul')!;
 
     socket.on('connect', () => {
         serverStatusLabel.innerHTML = 'Online';
@@ -18,5 +22,35 @@ const addListeners = (socket: Socket) => {
 
     socket.on('disconnect', () => {
         serverStatusLabel.innerHTML = 'Offline';
+    })
+
+    socket.on('clients-updated', (clients: string[]) => {
+        let clientsHtml = '';
+        clients.forEach(clientId => {
+            clientsHtml += `<li>${clientId}</li>`;
+        })
+
+        clientsUl.innerHTML = clientsHtml;
+    })
+
+    messageForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        if (messageInput.value.trim().length <= 0) return;
+
+        socket.emit('message-from-client', {
+            id: socket.id,
+            message: messageInput.value
+        });
+
+        messageInput.value = '';
+    })
+
+    socket.on('message-from-server', (payload: { fullName: string, message: string }) => {
+        const messageLi = document.createElement('li');
+        messageLi.innerHTML = `
+            <strong>${payload.fullName}</strong>: ${payload.message}
+        `;
+        messagesUl.appendChild(messageLi);
     })
 }
